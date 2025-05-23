23af593df80637a4c7201a1098922d811724399f
tom-ard <tom@arducam.com>
2025-05-23 09:49:40 +0800

    Update config file
    1. 修复ros程序读取custom_config.yaml路径错误的问题.
    2. custom_config.yaml增加手动设置曝光和增益.

--- ros-example/catkin_ws/src/oakchina_vio_package/src/main.cpp
+++ ros-example/catkin_ws/src/oakchina_vio_package/src/main.cpp
@@ -104,7 +104,7 @@ tf2::Quaternion rotationMatrixToQuaternion(const double* R) {
             q.setZ(0.25 * s);
         }
     }
-
+    q.normalize();
     return q;
 }
 
@@ -204,33 +204,19 @@ int main(int argc, char **argv) {
     signal(SIGINT,signal_handle);
 
     char c;
-
+    int badParams = 0;
     std::string custom_config_path = "./custom_config.yaml";
-    std::string custom_config;
-    std::ifstream configFile(custom_config_path, std::ios::in);
-    if (configFile.is_open()) {
-        std::stringstream buffer;
-        buffer << configFile.rdbuf();
-        configFile.close();
-        custom_config = buffer.str();
-    }
-
-    carina_a1088_init(const_cast<char *>(custom_config.c_str()), "./database.bin");
-    carina_a1088_start(
-        CarinaA1088PoseCallBack,
-        CarinaA1088VsyncCallBack,
-        CarinaA1088ImuCallBack,
-        CarinaA1088CameraCallBack,
-        CarinaA1088PointsCallBack,
-        CarinaA1088EventCallBack
-    );
-    carina_a1088_resume();
+    std::string database_path = "./database.bin";
 
     ros::init(argc, argv, "vio");
-    ros::NodeHandle n;
+    ros::NodeHandle n("~");
 
-    // ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-    // ros::Publisher imu_pub = n.advertise<sensor_msgs::Imu>("imu/data", 10);
+    badParams += !n.getParam("custom_config_path", custom_config_path);
+    badParams += !n.getParam("database_path", database_path);
+
+    std::cout << custom_config_path << std::endl;
+    std::cout << database_path << std::endl;
+
     ros::Publisher pose_pub = n.advertise<geometry_msgs::PoseStamped>("pose", 10);
     ros::Publisher path_pub = n.advertise<nav_msgs::Path>("path", 10);
     ros::Publisher left_image_pub = n.advertise<sensor_msgs::Image>("left_gray_image", 10);
@@ -239,7 +225,6 @@ int main(int argc, char **argv) {
     nav_msgs::Path path;
     path.header.frame_id = "map";
 
-    
     auto now = std::chrono::system_clock::now();
     auto now_time_t = std::chrono::system_clock::to_time_t(now);
     std::tm now_tm = *std::localtime(&now_time_t);
@@ -256,32 +241,36 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    std::string custom_config;
+    std::ifstream configFile(custom_config_path, std::ios::in);
+    if (configFile.is_open()) {
+        std::stringstream buffer;
+        buffer << configFile.rdbuf();
+        configFile.close();
+        custom_config = buffer.str();
+    }
+
+    carina_a1088_init(const_cast<char *>(custom_config.c_str()), const_cast<char*>(database_path.c_str()));
+    carina_a1088_start(
+        CarinaA1088PoseCallBack,
+        CarinaA1088VsyncCallBack,
+        CarinaA1088ImuCallBack,
+        CarinaA1088CameraCallBack,
+        CarinaA1088PointsCallBack,
+        CarinaA1088EventCallBack
+    );
+    carina_a1088_resume();
+
     while (ros::ok()) {
         sensor_msgs::Imu imu_msg;
         geometry_msgs::PoseStamped pose_stamped;
         tf2::Quaternion quat;
         sensor_msgs::ImagePtr left_msg, right_msg;
-        // std_msgs::String msg;
-        // msg.data = "Hello, ROS!";
         {
             std::lock_guard<std::mutex> auto_lock(pose_mtx);
             if (pose_ts > 0) {
-                // std::cout << "pose ts: " << pose_ts << " pose: " <<
-                //           pose_data[12] << "\t" << pose_data[13]
-                //           << "\t" << pose_data[14] << std::endl;
-                // std::cout << "pose ts: " << pose_ts << " pose: ";
-                // for (int i = 0; i < 16; i++) {
-                //     std::cout << pose_data[i] << "\t";
-                // }
-                // std::cout << std::endl;
 
                 outFile << "position: " << pose_data[12] << "," << pose_data[13] << "," << pose_data[14] << "\n";
-
-                // double R[9] = {
-                //     pose_data[0], pose_data[1], pose_data[2],
-                //     pose_data[4], pose_data[5], pose_data[6],
-                //     pose_data[8], pose_data[9], pose_data[10]
-                // };
 
                 double R[9] = {
                     pose_data[0], pose_data[4], pose_data[8],
@@ -324,62 +313,23 @@ int main(int argc, char **argv) {
                 // std::cout << "imu ts: " << imu_ts << " data: " <<
                 //           imu_data[0] << ", " << imu_data[1] << ", " << imu_data[2] << ", " <<
                 //           imu_data[3] << ", " << imu_data[4] << ", " << imu_data[5] << std::endl;
-                // imu_msg.header.stamp = ros::Time::now(); // 当前时间
-                // imu_msg.header.frame_id = "map";
-                // imu_msg.linear_acceleration.x = imu_data[0]; // X 轴加速度
-                // imu_msg.linear_acceleration.y = imu_data[1]; // Y 轴加速度
-                // imu_msg.linear_acceleration.z = imu_data[2]; // Z 轴加速度（重力）
-
-                // imu_msg.angular_velocity.x = imu_data[3]; // X 轴角速度
-                // imu_msg.angular_velocity.y = imu_data[4]; // Y 轴角速度
-                // imu_msg.angular_velocity.z = imu_data[5]; // Z 轴角速度
-
-                // imu_msg.orientation.x = quat.x(); // 四元数 X
-                // imu_msg.orientation.y = quat.y(); // 四元数 Y
-                // imu_msg.orientation.z = quat.z(); // 四元数 Z
-                // imu_msg.orientation.w = quat.w(); // 四元数 W
-                // imu_msg.orientation_covariance = {-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
                 imu_ts = -1;
             }
         }
-
-        // {
-        //     std::lock_guard<std::mutex> auto_lock(vsync_mtx);
-        //     if (vsync_ts > 0) {
-        //         std::cout << "vsync ts: " << vsync_ts << std::endl;
-        //         vsync_ts = -1;
-        //     }
-        // }
-
-        // {
-        //     std::lock_guard<std::mutex> auto_lock(points_mtx);
-        //     if (points_ts > 0) {
-        //         std::cout << "points ts: " << points_ts << " data: " << points_data.points_orb.size() << "\t"
-        //                   << points_data.points_lk.size() << std::endl;
-
-        //         points_ts = -1;
-        //     }
-        // }
 
         {
             std::lock_guard<std::mutex> auto_lock(image_mtx);
             if (image_ts > 0) {
                 std::cout << "image_ts ts: " << image_ts << std::endl;
-                // cv::Mat left_img(height, width, CV_8UC1, left_image);
                 left_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", left_image).toImageMsg();
-                // cv::Mat right_img(height, width, CV_8UC1, right_image);
                 right_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", right_image).toImageMsg();
                 left_image_pub.publish(left_msg);
                 right_image_pub.publish(right_msg);
                 image_ts = -1;
-                // left_image = NULL;
-                // right_image = NULL;
             }
         }
 
-        // chatter_pub.publish(msg);
         pose_pub.publish(pose_stamped);
-        // imu_pub.publish(imu_msg);
 
         path_pub.publish(path);
         ros::spinOnce();
